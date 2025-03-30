4287d83ae583f60f09c1480ebbb6fb7d67821de2
TMS <tms@tmssmsmsmsmss.hu>
2025-03-30 10:46:25 -0400

    meal selection mapped in the conferecneEvent

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -201,9 +201,26 @@ const ConferenceEvent = () => {
                                 </div>
 
                                 <div className="input-container venue_selection">
-
+                                    <label htmlFor="NumberOfPeople"><h3>Number of People:</h3></label>
+                                    <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
+                                    //No of People changed to the value entered
+                                            onClick={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                                            min="1"
+                                    />
                                 </div>
                                 <div className="meal_selection">
+                                    {mealsItems.map((item, index) => (
+                                        <div className="meal_item" key={index} style={{padding: 15}}>
+                                            <div className="inner">
+                                                <input type="checkbox" id={ `meal_${index}`}
+                                                checked = {item.selected}
+                                                onChange={() => handleMealSelection(index)}
+                                                />
+                                                <label htmlFor={`meal_${index}`} > {item.name} </label>
+                                                </div>
+                                                <div className="meal_cost">${item.cost}</div>
+                                        </div>
+                                    ))}
 
                                 </div>
                                 <div className="total_cost">Total Cost: </div>
